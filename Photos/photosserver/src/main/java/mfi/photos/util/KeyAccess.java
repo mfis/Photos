32d0073076833db90319330ce22eca52f6be89a4
mfis <fischermatthias1980@googlemail.com>
2021-06-01 22:08:17 +0200

    KeyAccess streams try with resource

--- Photos/photosserver/src/main/java/mfi/photos/util/KeyAccess.java
+++ Photos/photosserver/src/main/java/mfi/photos/util/KeyAccess.java
@@ -4,6 +4,7 @@ import mfi.photos.shared.AES;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class KeyAccess {
@@ -40,22 +41,30 @@ public class KeyAccess {
 		return this.key != null && this.key.length > 0;
 	}
 
-	public String getKey() {
+	public String getKey(){
 		if (key == null) {
 			return null;
 		}
-		ByteArrayInputStream in = new ByteArrayInputStream(this.key);
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		AES.decrypt(getUptime().toCharArray(), in, out, 0, -1);
-		String key = new String(out.toByteArray(), StandardCharsets.UTF_8);
-		return key;
+		try(
+			final var in = new ByteArrayInputStream(this.key);
+			final var out = new ByteArrayOutputStream()){
+				AES.decrypt(getUptime().toCharArray(), in, out, 0, -1);
+				String key = new String(out.toByteArray(), StandardCharsets.UTF_8);
+				return key;
+		} catch (IOException e) {
+			throw new IllegalStateException("StreamHandling failed.", e);
+		}
 	}
 
 	public void setKey(String key) {
-		ByteArrayInputStream in = new ByteArrayInputStream(key.getBytes(StandardCharsets.UTF_8));
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		AES.encrypt(0, getUptime().toCharArray(), in, out);
-		this.key = out.toByteArray();
+		try(
+			final var in = new ByteArrayInputStream(key.getBytes(StandardCharsets.UTF_8));
+			final var out = new ByteArrayOutputStream()){
+				AES.encrypt(0, getUptime().toCharArray(), in, out);
+				this.key = out.toByteArray();
+		} catch (IOException e) {
+			throw new IllegalStateException("StreamHandling failed.", e);
+		}
 	}
 
 	public String getUptime() {
